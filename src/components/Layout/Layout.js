9c173f73cf1c89ec94c9f6293e530580ad2e3e3d
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-04-03 11:20:49 -0400

    layout style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Aux from '../../hoc/Aux' 
 import React from 'react'
+import style from './Layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={style.Content}>
             {props.children}
         </main>
     </Aux>
