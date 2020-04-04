82a8bc797dfb4f684bc2093002ab21635c2a53a2
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-04-04 13:43:58 -0400

    update ingredients and price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,10 +2,10 @@ import React from 'react'
 import style from './BuildControl.css'
 
 const buildControl = (props) => (
-    <div>
-        <div className={style.BuildControl}>{props.label}</div>
-        <button className={style.Less}>Less</button>
-        <button className={style.More}>More</button>
+    <div className={style.BuildControl}>
+        <div>{props.label}</div>
+        <button className={style.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={style.More} onClick={props.added}>More</button>
     </div>
 )
 
