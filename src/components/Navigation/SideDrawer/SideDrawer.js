317cd25742d7cd0b6821b5ff74c2d1c19c203e29
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-04-06 09:41:35 -0400

    sidedrawer backdrop improvements

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,31 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import style from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
+    // Dynamic creation of class styles to show or hide the 
+    // side drawer when the backdrop is clicked
+    let attachedClasses = [style.SideDrawer, style.Close]
+    if (props.open) {
+        attachedClasses = [style.SideDrawer, style.Open]
+    }
+
     return (
-        <div className={style.SideDrawer}>
-            <div className={style.Logo}>
-                <Logo/>
+        <Aux>
+            {/* props.closed & props.open are provided by layout.js */}
+            <Backdrop show={props.open} clicked={props.closed}/>
+            {/* Dynamic styles in action */}
+            <div className={attachedClasses.join(' ')}>
+                <div className={style.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
